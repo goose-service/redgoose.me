082988cc1d2780cf89790fe7e014f9b75a268628
redgoose <scripter@me.com>
2016-10-31 22:42:57 +0900

    본문 스타일 수정

--- view/layout.blade.php
+++ view/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="ko">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="viewport" content="user-scalable=yes, initial-scale=1.0, maximum-scale=2.0, minimum-scale=1.0">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
 	<title>{{ $title }}</title>
 
 	<meta name="google-site-verification" content="{{ $pref['googleVerify'] }}" />
