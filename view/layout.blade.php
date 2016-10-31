cc71dcbbf040d070ff13137fb97fc33fd74be58e
redgoose <scripter@me.com>
2016-10-31 23:42:47 +0900

    add google analytics

--- view/layout.blade.php
+++ view/layout.blade.php
@@ -87,5 +87,15 @@
 <script src="{{ __ROOT__ }}/assets/dist/js/vendor.js"></script>
 <script src="{{ __ROOT__ }}/assets/dist/js/app.js"></script>
 @yield('script')
+<script>
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-42563094-1', 'redgoose.me');
+ga('send', 'pageview');
+</script>
 </body>
 </html>
