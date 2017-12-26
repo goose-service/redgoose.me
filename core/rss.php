a38121aea8df73c70dac81ea31e24e6b94fe6676
redgoose <scripter@me.com>
2017-12-27 07:33:49 +0900

    rss부분 작업

--- core/rss.php
+++ core/rss.php
@@ -1,1 +1,73 @@
-rss.php
+<?php
+if(!defined("__GOOSE__")){exit();}
+
+
+$countPerPage = 15;
+
+
+/**
+ * @var array $_params
+ * @var API $api
+ * @var object $pref
+ */
+$data = $api->index((object)[
+	'app_srl' => __APP_SRL__,
+	'page' => 1,
+	'print_data' => 'article',
+	'size' => $countPerPage ? $countPerPage : __DEFAULT_ITEM_COUNT__,
+]);
+
+
+// init parsedown
+require_once(__GOOSE_PWD__ . 'vendor/Parsedown/Parsedown.class.php');
+$Parsedown = new Parsedown();
+
+
+// edit articles
+foreach($data->articles as $k=>$o)
+{
+	if ($o['json']['mode'] === 'markdown')
+	{
+		$data->articles[$k]['content'] = $Parsedown->text($o['content']);
+	}
+
+	if (isset($data->articles[$k]['json']['thumbnail']['srl']))
+	{
+		$file = core\Spawn::item([
+			'table' => core\Spawn::getTableName('File'),
+			'where' => 'srl=' . (int)$data->articles[$k]['json']['thumbnail']['srl']
+		]);
+
+		$data->articles[$k]['content'] = (
+			'<img src="' . __GOOSE_URL__ . '/' . $file['loc'] . '" alt="' . $file['name'] . '">'.
+			'<p>'.
+			mb_substr(strip_tags(preg_replace('~>\\s+<~m', '><', $data->articles[$k]['content'])), 0, 150, 'utf-8').
+			'...</p>'
+		);
+	}
+	else
+	{
+		$data->articles[$k]['content'] = (
+			'<p>'.
+			mb_substr(strip_tags(preg_replace('~>\\s+<~m', '><', $data->articles[$k]['content'])), 0, 150, 'utf-8').
+			'...</p>'
+		);
+	}
+
+	$data->articles[$k]['date'] = date("D, d M Y H:i:s O", strtotime($o['regdate_original']));
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+
+// set header
+//header('Content-Type: application/rss+xml; charset=utf-8');
+header('Content-Type: application/xml; charset=utf-8');
+
+
+// render page
+$blade->render('rss', [
+	'pref' => $pref,
+	'articles' => $data->articles,
+]);
