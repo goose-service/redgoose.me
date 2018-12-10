366d33d8947ed11bd8fbe9d80fdb28fba4ae03c3
redgoose <scripter@me.com>
2018-12-10 17:13:10 +0900

    스타일 추가수정

--- view/work/body.blade.php
+++ view/work/body.blade.php
@@ -14,7 +14,7 @@ if(!defined("__GOOSE__")){exit();}
 					@if($data->nest_name)<span>{{ $data->nest_name }}</span>@endif
 					@if($data->category_name)<span>{{ $data->category_name }}</span>@endif
 				</p>
-				<h1>{{ ($data->title == '.') ? 'Untitled work' : $data->title }}</h1>
+				<h2>{{ ($data->title == '.') ? 'Untitled work' : $data->title }}</h2>
 			</header>
 
 			<div class="workBody work__body">
