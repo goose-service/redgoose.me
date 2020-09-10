f0239daa61041c87a3016cd408c3757493efd88e
redgoose <scripter@me.com>
2020-09-10 22:51:19 +0900

    텍스트 셔플 적용

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 import $ from 'cash-dom';
+import shuffle from 'auto-writer/src/shuffle';
 
 /**
  * toggle category on mobile
@@ -13,7 +14,28 @@ function toggleCategoryNavigation()
 	});
 }
 
+function mouseOverItems()
+{
+	const $works = $('.index-works a');
+	$works.each(function() {
+		$(this).on('mouseenter', function() {
+			if ($(window).width() < 768) return true;
+			const $caption = $(this).find('.index-work__caption');
+			const $elements = $caption.find('strong, em');
+			$elements.each(function(n) {
+				const self = this;
+				setTimeout(() => shuffle(self, {
+					text: self.innerText,
+					pattern: 'abcdefghijklmnopqrstuvwxyz0123456789-_!@#$%^&*()+~<>ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㄲㄸㅃㅆㅉ',
+					randomTextType: n === 0 ? 'pattern' : 'unicode',
+				}), 180 * n);
+			});
+		});
+	});
+}
+
 export default function()
 {
 	toggleCategoryNavigation();
+	mouseOverItems();
 }
