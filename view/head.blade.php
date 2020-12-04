bef06ff088e8d8b7600def0d6cf0e3fec2599105
redgoose <scripter@me.com>
2020-12-04 18:43:35 +0900

    google 트래커 삭제

--- view/head.blade.php
+++ view/head.blade.php
@@ -5,7 +5,6 @@
 <meta name="keywords" content="redgoose 2d 3d photography graphic design case-study programing web 붉은거위 거위 portfolio drawing"/>
 <link rel="canonical" href="{{__URL__}}{{$_SERVER['REQUEST_URI']}}"/>
 
-<meta name="google-site-verification" content="bF0pYg2C9_CT16xoUenLKPFbGDG-LNYJoV2vAOpf-74"/>
 <meta name="p:domain_verify" content="3e05cc5b868de908660cf4004dbdc8fe"/>
 
 <meta property="og:site_name" content="redgoose"/>
@@ -27,11 +26,3 @@
 
 <link rel="stylesheet" href="{{__ROOT__}}/assets/dist/app.css" media="screen"/>
 <link rel="manifest" href="{{__ROOT__}}/manifest.json"/>
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-42563094-1"></script>
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-42563094-1');
-</script>
