38a29fd9a6e46c4c56cfc0bcb1e008722988e758
redgoose <scripter@me.com>
2021-11-14 22:22:22 +0900

    edit meta tags

--- view/head.blade.php
+++ view/head.blade.php
@@ -4,25 +4,20 @@
 <meta name="author" content="redgoose"/>
 <meta name="keywords" content="redgoose 2d 3d photography graphic design case-study programing web 붉은거위 거위 portfolio drawing"/>
 <link rel="canonical" href="{{__URL__}}{{$_SERVER['REQUEST_URI']}}"/>
-
-<meta name="p:domain_verify" content="3e05cc5b868de908660cf4004dbdc8fe"/>
-
 <meta property="og:site_name" content="redgoose"/>
 <meta property="og:url" content="{{__URL__}}{{ $_SERVER['REQUEST_URI'] }}"/>
 <meta property="og:locale" content="ko_KR"/>
 <meta property="og:type" content="website"/>
 <meta property="fb:app_id" content="1619661748331088"/>
-
 <meta name="apple-mobile-web-app-capable" content="yes"/>
 <meta name="mobile-web-app-capable" content="yes"/>
-<meta name="apple-mobile-web-app-status-bar-style" content="light"/>
-
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="theme-color" content="currentColor">
+<meta name="p:domain_verify" content="3e05cc5b868de908660cf4004dbdc8fe"/>
 @yield('meta')
-
 <link rel="shortcut icon" href="{{__ROOT__}}/favicon.ico"/>
 <link rel="icon" type="image/x-icon" href="{{__ROOT__}}/assets/images/redgoose_256x256.png"/>
 <link rel="apple-touch-icon" href="{{__ROOT__}}/assets/images/redgoose_256x256.png"/>
 <link rel="apple-touch-startup-image" href="{{__ROOT__}}/assets/images/redgoose_256x256.png"/>
-
 <link rel="stylesheet" href="{{__ROOT__}}/assets/dist/app.css" media="screen"/>
 <link rel="manifest" href="{{__ROOT__}}/manifest.json"/>
