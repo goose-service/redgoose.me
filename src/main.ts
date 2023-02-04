761d2c933217a38ddb2f57b6f1df3db2ee3306d8
redgoose.eth <scripter@me.com>
2023-02-04 21:52:47 +0900

    pwa 관련 조정작업

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,8 @@ import './assets/scss/main.scss'
 window.app = new Main({
   target: <HTMLElement>(document.getElementById('app')),
 })
+
+// setup service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register("/sw.js")
+}
