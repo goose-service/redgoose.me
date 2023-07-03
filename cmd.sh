794f5c1a9a84e076c74906429bc7ef62eaea565b
redgoose.eth <scripter@me.com>
2023-07-03 13:03:44 +0900

    잡다한 부분들 수정

--- cmd.sh
+++ cmd.sh
@@ -2,17 +2,22 @@ script="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 
 case "$1" in
 
+  build)
+    docker buildx build --platform=linux/amd64 -t redgoose/redgoose.me:latest .
+    ;;
+
   upload)
-    docker save redgoose/redgoose.me:latest | ssh -C goose@redgoose.me 'cd www && docker load'
+    docker save redgoose/redgoose.me:latest | ssh -C redgoose.me 'cd ~/docker/www && docker load'
     ;;
 
   upgrade)
     docker buildx build --platform=linux/amd64 -t redgoose/redgoose.me:latest .
-    docker save redgoose/redgoose.me:latest | ssh -C goose@redgoose.me 'cd www && docker-compose down && docker load && docker-compose up -d && cd ../service && ./cmd.sh service reload'
+    docker save redgoose/redgoose.me:latest | ssh -C goose@redgoose.me 'cd ~/docker/www && docker-compose down && docker load && 
+docker-compose up -d && cd ../service && ./cmd.sh service reload'
     ;;
 
   *)
-    echo "Usage: ${script} {upload|upgrade}" >&2
+    echo "Usage: ${script} {build|upload|upgrade}" >&2
     exit 3
     ;;
 
